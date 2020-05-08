7d60d15a69b75d8f50fbdd2c76c9bec03ecd41ea
Rax Ixor <raxixor@rax.ee>
2020-05-08 11:37:12 +0300

    feat: :construction: working on getting everything actually working

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 #[macro_use]
 extern crate smart_default;
+use std::env;
 
 mod iqdb;
 mod util;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        main_input()
+    } else {
+        let res = util::build_match(args[1].as_ref());
+    }
+}
+
+fn main_input() {
+    let args: String = dialoguer::Input::<String>::new()
+        .allow_empty(false)
+        .with_prompt("Link")
+        .interact()
+        .unwrap();
+
+    let res = util::build_match(args.as_ref());
 }
--- src/util.rs
+++ src/util.rs
@@ -1,11 +1,32 @@
 use crate::iqdb::Matches;
+use std::error::Error;
+use std::fmt;
 
 fn check_url(url: &str) -> bool {
-    let head = ureq::head(url);
+    let head = ureq::head(url).call();
 
     head.header("content-type").unwrap_or("").contains("image")
 }
 
-fn build_match(url: &str) -> Matches {
-    Matches::default()
+pub fn build_match(url: &str) -> Result<Matches, Box<dyn Error>> {
+    if !check_url(url) {
+        return Err(Box::new(BuildError(format!("invalid url: {}", url))));
+    }
+
+    Ok(Matches::default())
+}
+
+#[derive(Debug, Clone)]
+struct BuildError(String);
+
+impl fmt::Display for BuildError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl Error for BuildError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
 }
